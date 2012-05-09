1967c1a1f66711debdaf4baa2bd4753425f09d09
Ming Iu <git@babylscript.com>
2012-05-08 21:18:18 -0400

    Playing with continuations to try and understand them.

--- src/org/mozilla/javascript/gwt/Test.java
+++ src/org/mozilla/javascript/gwt/Test.java
@@ -1,7 +1,9 @@
 package org.mozilla.javascript.gwt;
 
+import org.mozilla.javascript.BaseFunction;
 import org.mozilla.javascript.CompilerEnvirons;
 import org.mozilla.javascript.Context;
+import org.mozilla.javascript.ContinuationPending;
 import org.mozilla.javascript.Evaluator;
 import org.mozilla.javascript.InterpretedFunction;
 import org.mozilla.javascript.Interpreter;
@@ -11,24 +13,157 @@ import org.mozilla.javascript.Script;
 import org.mozilla.javascript.ScriptOrFnNode;
 import org.mozilla.javascript.ScriptRuntime;
 import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+import org.mozilla.javascript.Undefined;
 import org.mozilla.javascript.debug.DebuggableScript;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Window;
 
 public class Test implements EntryPoint 
 {
 
+    static class AlertFunction extends BaseFunction
+    {
+        public Object call(Context cx, Scriptable scope, Scriptable thisObj, Object[] args)
+        {
+            if (args.length > 0)
+                alertPassthrough("" + args[0]);
+            return null;
+        }
+        public static native void alertPassthrough(String msg) /*-{
+            $wnd.alert(msg);
+        }-*/;
+    }
+
+    static class ContinuationTestFunction extends BaseFunction
+    {
+        public Object call(Context cx, Scriptable scope, Scriptable thisObj, Object[] args)
+        {
+            System.out.println("enter continuation");
+            ContinuationPending pending = cx.captureContinuation();
+            throw pending;
+        }
+    }
+    
+    // TODO: This might be called fairly often, so it might be
+    // useful to rewrite this so that it doesn't use exceptions
+    // for continuations
+    static class AsynchronousToBlockingFunction extends BaseFunction
+    {
+        // NOTE: This code is NOT reentrant, so don't call back into JavaScript
+        
+        // Override this with the code for starting the asynchronous operation
+        protected void startCall(Context cx, Scriptable scope, Scriptable thisObj, Object[] args)
+        {
+            
+        }
+        
+        // Override this with the code that checks if the asynchronous operation
+        // is done. Return true iff the operation is done.
+        protected boolean isCallDone(Context cx, Scriptable scope, Scriptable thisObj, Object[] args)
+        {
+            return true;
+        }
+        
+        // After the operation is complete, this function will be executed
+        // to get the result of the asynchronous operation
+        protected Object getCallResult(Context cx, Scriptable scope, Scriptable thisObj, Object [] args)
+        {
+            return null;
+        }
+        
+        Context savedCx;
+        Scriptable savedScope;
+        Scriptable savedThisObj;
+        Object [] savedArgs;
+        Object savedResult;
+        public Object call(Context cx, Scriptable scope, Scriptable thisObj, Object[] args)
+        {
+            savedCx = cx;
+            savedScope = scope;
+            savedThisObj = thisObj;
+            savedArgs = args;
+            
+            startCall(savedCx, savedScope, savedThisObj, savedArgs);
+            if (!isCallDone()) 
+            {
+                ContinuationPending pending = cx.captureContinuation();
+                pending.setApplicationState(this);
+                throw pending;
+            }
+            return getResult();
+        }
+        public boolean isCallDone()
+        {
+            boolean isDone = isCallDone(savedCx, savedScope, savedThisObj, savedArgs);
+            if (isDone) savedResult = getCallResult(savedCx, savedScope, savedThisObj, savedArgs); 
+            return isDone;
+        }
+        public Object getResult()
+        {
+            return savedResult;
+        }
+        
+    }
+    
+    public static class AsynchronousBlockingReturns extends JavaScriptObject
+    {
+        protected AsynchronousBlockingReturns() {}
+        final public native boolean isDone() /*-{
+            return this.done();
+        }-*/;
+        final public native JavaScriptObject result() /*-{
+            return this.result();
+        }-*/; 
+    }
+    
+    public static class SleepFunction extends AsynchronousToBlockingFunction
+    {
+        protected void startCall(Context cx, Scriptable scope, Scriptable thisObj, Object[] args)
+        {
+            fns = sleep(5000).cast();
+        }
+        protected boolean isCallDone(Context cx, Scriptable scope, Scriptable thisObj, Object[] args)
+        {
+            return fns.isDone();
+        }
+        protected Object getCallResult(Context cx, Scriptable scope, Scriptable thisObj, Object [] args)
+        {
+            return fns.result();
+        }
+        AsynchronousBlockingReturns fns;
+        public static native JavaScriptObject sleep(int millis) /*-{
+            var isDone = false;
+            var toReturn = null;
+            var handle = $wnd.setTimeout(function() {
+                    isDone = true;
+                }, millis);
+            return {
+                    done: function() { return isDone; },
+                    result: function() { return toReturn; }
+                };
+        }-*/;
+    }
+    
+    Context cx;
+    Scriptable scope;
+    
     public void onModuleLoad() 
     {
-        Scriptable scope;
-        Context cx = Context.enter();
+        cx = Context.enter();
         cx.setOptimizationLevel(-1);
         
         String result = "";
         try {
             scope = cx.initStandardObjects();
+            ScriptableObject.putProperty(scope, null, "alert", new AlertFunction());
+            ScriptableObject.putProperty(scope, null, "continuationTest", new ContinuationTestFunction());
+            ScriptableObject.putProperty(scope, null, "sleep", new SleepFunction());
 //            result = Context.toString(cx.evaluateString(scope, "22", "<test>", 0, null));
 
             CompilerEnvirons compilerEnv = new CompilerEnvirons(); 
@@ -37,7 +172,8 @@ public class Test implements EntryPoint
 //                p.calledByCompileFunction = true;
 //            }
 //             tree;
-            String sourceString = "22 + 23; ";
+//            String sourceString = "22 + 23; alert('hi'); continuationTest()";
+            String sourceString = "22 + 23; result = sleep(5000); alert(result);";
             int lineno = 0;
             String sourceName = "hey";
             if (sourceString != null) {
@@ -52,11 +188,20 @@ public class Test implements EntryPoint
 
                 InterpretedFunction resultScript;
                 resultScript = (InterpretedFunction) compiler.createScriptObject(bytecode, null);
-                
-                result = "" + resultScript.exec(cx, scope);
+                try {
+                    result = "" + cx.executeScriptWithContinuations(resultScript, scope);
+                } catch (ContinuationPending pending) {
+                    if (pending.getApplicationState() instanceof AsynchronousToBlockingFunction)
+                    {
+                        restartContinuationInTimer(pending);
+
+                    }
+                    else
+                        result = "" + cx.resumeContinuation(pending.getContinuation(), scope, 5);
+                }
+//                result = "" + resultScript.exec(cx, scope);
 
             } 
-
 
             
 //public final Object evaluateString(Scriptable scope, String source,
@@ -73,11 +218,29 @@ public class Test implements EntryPoint
 //}
             
         } finally {
-//            Context.exit();
+            Context.exit();
         }
 
         
         Element el = Document.get().getElementById("fillin");
         el.setInnerHTML(result);
     }
+
+    void restartContinuationInTimer(final ContinuationPending pending)
+    {
+        Timer t = new Timer() {
+            public void run() {
+                AsynchronousToBlockingFunction async = (AsynchronousToBlockingFunction)pending.getApplicationState();
+                if (async.isCallDone())
+                {
+                    Window.alert("" + cx.resumeContinuation(pending.getContinuation(), scope, async.getResult()));
+                }
+                else
+                    Window.alert("nothing");
+            }
+        };
+        t.schedule(6000);
+
+    }
+
 }
